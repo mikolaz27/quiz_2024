246f24915b9f3675ba5890618f056e130ec9fb4a
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2024-05-07 21:26:53 +0300

    Added different envs

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -4,14 +4,14 @@ SECRET_KEY = "django-insecure-#dlv8***nlb#xf3u32a$psx#_at(@j@9(yoaonefyq7%-gw*w$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # NOQA
 
-INSTALLED_APPS += []
+INSTALLED_APPS += []  # NOQA
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
 }
 
